7168c6757f75085291862cad60b59554a7241dc5
MichaelNarkus <michael.narkus@stud.h-da.de>
2018-06-28 08:22:29 +0200

    Added check for compodoc documentation

---
+++ .travis/compodoc-check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Path to this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Path the script was called from
+IPWD="$(pwd)"
+# Import shared vars
+. ${DIR}/_shared-vars.sh
+
+
+echo
+echo "+++ Check Frontend-Doc +++"
+echo
+
+if ([ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "develop" ]) || ([ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]); then
+  echo "+ detected pull request from ($TRAVIS_PULL_REQUEST_BRANCH) to $TRAVIS_BRANCH"
+  cd app
+  cd webFrontend
+  npm run compodoc
+else
+  echo -e "${YELLOW}+ WARNING: No Pull Request agiainst Develop or Master -> skipping automate frontend doc check${NC}";
+fi
